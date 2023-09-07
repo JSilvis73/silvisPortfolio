d14d0889b728ad0f4523d789fb3c34885cc5ad46
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-06 21:48:04 -0400

    Styled the personal page to have a circular avatar. Also added margin from top.

--- src/pages/personal/personal.jsx
+++ src/pages/personal/personal.jsx
@@ -5,19 +5,20 @@ import avatarDefault from '../../assets/images/avatarDefault.jpg';
 
 export const Personal = () => {
     return (
-        <div className="personalContainer gradient">
+        <div className="personalContainer marginTop15">
             <header>
-                <h1>Personal</h1>
+                <h1>Jason Silvis</h1>
             </header>
 
-            <main>
-                <div className="avatarContainer">
+            <main className="mainContainer ">
+                <div className="avatarContainer ">
                     <Avatar 
                     img={avatarDefault}
                     />
                     
                 </div>
-                <div className="aboutMeContainer centerText">
+
+                <div className="aboutMeContainer centerText marginTop15">
                     This section will be about me.
                 </div>
             </main>

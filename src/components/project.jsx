5f70e3b48e18373ee6545e435d882e7b7adb4005
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-07 22:41:42 -0400

    Working on styling the Project component. Felt like a great time to commit. Need to format pictures, but added borders to images and shape to cards.

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,13 +4,13 @@ import '../pages/projects/projects.css';
 export const Project = (props) => {
 
     return (
-        <div className="projectWrapper displayFlexColCenter testBorder">
-            
-            <h3 className="projectHeader marginTop10perc">{props.name}</h3>
-            <img className="projectImage marginTop10perc" src={props.image} />
-            <p>{props.desc}</p>
-            <a className="projectURL" href={props.url}>{props.url}</a>
-           
+        <div className="projectContainer ">
+            <div className="projectWrapper displayFlexColCenter marginTop10perc">
+                <h3 className="projectHeader marginTop10perc">{props.name}</h3>
+                <img className="projectImage marginTop10perc" src={props.image} />
+                 <p>{props.desc}</p>
+                 <a className="projectURL" href={props.url}>{props.url}</a>
+            </div>
         </div>
     )
 }

8922c16ebb30dd863041e172438dfbecb33e36aa
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-11 19:48:26 -0400

    Added a shake animation to the project cards. Thanks ChatGPT!

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import '../pages/projects/projects.css';
 
 export const Project = (props) => {
+ const [isShaking, setIsShaking] = useState(false);
+
+ const startShaking = () => {
+    setIsShaking(true);
+    setTimeout(() => setIsShaking(false), 1000);
+ }
+
 
     return (
-       <div className="projectContainer ">
-            <div className="projectWrapper displayFlexColCenter marginTop10perc">
+       <div className={`projectContainer`}>
+            <div className={`projectWrapper displayFlexColCenter marginTop10perc  ${isShaking ? 'shake-animation' : ''}`}  onMouseEnter={startShaking}>
                 <h5 className="projectHeader">{props.name}</h5>
                 <img className="projectImage" src={props.image} />
                 <div className="card-inner displayFlexColCenter">

634c5bcf87fbc511e95912177963ad52e9df9eb0
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-11 15:40:35 -0400

    Removed some redundant code from css

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,9 +6,9 @@ export const Project = (props) => {
     return (
        <div className="projectContainer ">
             <div className="projectWrapper displayFlexColCenter marginTop10perc">
-                <h3 className="projectHeader marginTop10perc">{props.name}</h3>
-                <img className="projectImage marginTop10perc" src={props.image} />
-                 <p className="marginTop10perc height50px">{props.desc}</p>
+                <h3 className="projectHeader">{props.name}</h3>
+                <img className="projectImage" src={props.image} />
+                 <p >{props.desc}</p>
                  <a className="projectURL" href={props.url}>{props.urlDesc}</a>
             </div>
         </div> 

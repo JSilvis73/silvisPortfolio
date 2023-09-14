dc4c65bf009b14256bf16262b62df363d4d929aa
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-14 07:54:18 -0400

    Fixed the image sizing bug that would distort the image when scrolled on carousel.

--- src/pages/personal/personal.jsx
+++ src/pages/personal/personal.jsx
@@ -44,7 +44,7 @@ export const Personal = () => {
             </div>
             
             
-            <div id="carouselExampleCaptions" class="carousel slide ">
+            <div id="carouselExampleCaptions" class="carousel slide">
                 <div class="carousel-indicators">
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -52,7 +52,7 @@ export const Personal = () => {
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active ">
-                <img src={fEndLanguage} class="d-block w-100"  alt="Font-end languages logo"/>
+                <img src={fEndLanguage} class="d-block w-100" alt="Font-end languages logo"/>
                 <div class="carousel-caption d-none d-md-block">
                   <h5> Coding Languages</h5>
                   <p>Unlock the potential of your online presence with expertise in HTML, CSS and JavaScript.</p>

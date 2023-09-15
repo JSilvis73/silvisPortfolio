929f457e3c6b1a0e4880d804a72f9efc0254fc1c
Jason Silvis <JSilvis030592@yahoo.com>
2023-09-14 23:37:48 -0400

    Working on styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css';
 
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg text-dark">
+        <nav class="navbar navbar-expand-lg text-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">Silvis' Portfolio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,7 +13,7 @@ export const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="/projects">Projects</a>
